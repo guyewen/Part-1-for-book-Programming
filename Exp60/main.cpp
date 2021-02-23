4919d89471e014c60c90f53a49cf527d92fe30a1
Yewen Gu <Yewen.Gu@nhh.no>
2021-02-23 16:07:04 +0100

    Update Exp60

--- Exp60/main.cpp
+++ Exp60/main.cpp
@@ -7,7 +7,7 @@ int main()
     cout << "Please enter your first name (followed by 'enter'):\n";
     string first_name; // define a string type variable named "first_name"
     cin >> first_name; // read input into defined variable "first_name"
-    cout << "Hello, " << first_name << "!\n" << endl; // output Hello + first_name
-    cout << "first_name" << " is " << first_name; // difference between "first_name" and first_name
+    cout << "Hello, " << first_name << endl; // output Hello + first_name
+    cout << "first_name" << " is " << first_name << "!\n" << endl; // difference between "first_name" and first_name
     return 0;
 }
