adea7da8a6d6354ea4d40ffa9244e1634352aeaa
Yewen Gu <Yewen.Gu@nhh.no>
2021-02-23 12:59:03 +0100

    Update the example on page 45

---
+++ HelloWord-P45/main.cpp
@@ -0,0 +1,13 @@
+// Example on page 45 of book programming. This program outputs the message "Hello, World!" to the monitor
+
+#include "std_lib_facilities.h" // The header file created for the book Programming
+
+// The following two lines can replace #include "std_lib_facilities.h"
+// #include <iostream>
+// using namespace std;
+
+int main()
+{
+    cout << "Hello, World!\n" << endl;
+    return 0;
+}
