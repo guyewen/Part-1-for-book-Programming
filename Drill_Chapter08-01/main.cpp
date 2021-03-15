dc68bbbcdcaa5174a67398f4926057b5b5674c82
Yewen Gu <Yewen.Gu@nhh.no>
2021-03-15 16:31:25 +0100

    Create Drill08-01

---
+++ Drill_Chapter08-01/main.cpp
@@ -0,0 +1,17 @@
+// Chapter 8, drill 01
+// use.cpp
+
+#include "my.h"
+
+int main()
+{
+ //assign 7 to foo
+ foo = 7;
+
+ //print what's in foo
+ print_foo();
+
+ //send an integer to be printed to screen
+ print(99);
+
+}
---
+++ Drill_Chapter08-01/my.h
@@ -0,0 +1,6 @@
+// Chapter 8, drill 01
+// my.h
+
+extern int foo;
+void print_foo();
+void print(int);
